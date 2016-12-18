c3de954282f880990794573a1aa8ef599b529a1f
baiyidjp <271524878@qq.com>
2016-12-18 11:30:03 +0800

    Initial Commit

---
+++ AFNetworkManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkManager
+//
+//  Created by Keep丶Dream on 16/12/18.
+//  Copyright © 2016年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AFNetworkManager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AFNetworkManager
+//
+//  Created by Keep丶Dream on 16/12/18.
+//  Copyright © 2016年 dong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
